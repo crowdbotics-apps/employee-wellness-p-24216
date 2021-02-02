285ce5c959c9c9c6859f8134972eb6052ee73c2d
Crowdbotics <team@crowdbotics.com>
2021-02-02 20:14:31 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen18199775', human_name: 'Blank Screen 18', access_route: 'BlankScreen18199775'},
 { name: 'ArticleList199774', human_name: 'Article List', access_route: 'ArticleList199774'},
 { name: 'Maps199761', human_name: 'Maps', access_route: 'Maps199761', icon: 'map'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen18199775Navigator from '../features/BlankScreen18199775/navigator';
 import ArticleList199774Navigator from '../features/ArticleList199774/navigator';
 import Maps199761Navigator from '../features/Maps199761/navigator';
 
@@ -15,6 +16,7 @@ import Maps199761Navigator from '../features/Maps199761/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen18199775: { screen: BlankScreen18199775Navigator },
 ArticleList199774: { screen: ArticleList199774Navigator },
 Maps199761: { screen: Maps199761Navigator },
 
